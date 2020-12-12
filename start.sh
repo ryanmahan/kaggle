f9404c398ce079fad90c557ca04e613b48476367
Ryan Mahan <ryanmahan97@gmail.com>
2020-12-12 13:24:33 -0500

    tweaked shampoo NN architecture

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export $(egrep -v '^#' .env | xargs)
 
-docker run --gpus all --env-file ./.env -it -p 8888:8888 -p 6006:6006 -v $(pwd):/notebooks tf:latest jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --notebook-dir=/notebooks
+docker run --gpus all --env-file ./.env -it -p 8888:8888 -p 6006:6006 -v $(pwd):/notebooks tf:latest \
+jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --notebook-dir=/notebooks --NotebookApp.token=$PASSWORD
